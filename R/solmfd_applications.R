e42d66a613049a9edf1d528fbbd1a2954b8228fc
wldyddl5510 <wldyddl5510@naver.com>
2021-11-21 10:57:10 -0600

    function skeletons (incomplete)

---
+++ R/solmfd_applications.R
@@ -0,0 +1,6 @@
+# solving constraint likelihood function for the solution manifold.
+# @params:
+# @return:
+const_likelihood = function() {
+  # TODO: solve constraint likelihood on solution manifold
+}
---
+++ R/solmfd_ftns.R
@@ -0,0 +1,13 @@
+# point clouds sampling in the solution manifold
+# @params:
+# @returns:
+sol_mfd_points = function() {
+  # TODO: Sampling point clouds in the solution manifold
+}
+
+# prior distribution sampling in the solution manifold
+# @params:
+# @returns:
+prior_on_solmfd = function() {
+
+}
